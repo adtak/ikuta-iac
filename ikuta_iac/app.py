dd7d3f4b47079e9d3230316ff1b9675d69d894c9
adtak <adtak62@gmail.com>
2021-07-28 23:56:31 +0900

    feat: add stack to app.py

--- ikuta_iac/app.py
+++ ikuta_iac/app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
+from ikuta_iac.stack.aws_ecr import IkutaEcrStack
+from ikuta_iac.stack.aws_lambda import IkutaLambdaStack
+
 
 app = core.App()
+
+ecr_stack = IkutaEcrStack(app, 'ikutaEcrStack')
+lambda_stack = IkutaLambdaStack(app, 'ikutaLambdaStack', repository=ecr_stack.ikuta_repository)
 
 app.synth()
--- ikuta_iac/stack/aws_lambda.py
+++ ikuta_iac/stack/aws_lambda.py
@@ -1,10 +1,11 @@
 from aws_cdk import core
 from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy
 from aws_cdk.aws_lambda import Code, Function, Handler, Runtime
+from aws_cdk.aws_ecr import IRepository
 
 
 class IkutaLambdaStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, repository: IRepository, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         lambda_exec_role = Role(
@@ -13,14 +14,12 @@ class IkutaLambdaStack(core.Stack):
             role_name='ikutaLambdaExecRole',
             assumed_by=ServicePrincipal('lambda.amazonaws.com'),
             managed_policies=[
-                ManagedPolicy.from_aws_managed_policy_name(
-                    'service-role/AWSLambdaBasicExecutionRole'
-                )
+                ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole'),  # noqa: E501
             ],
         )
 
         lambda_code = Code.from_ecr_image(
-            kwargs['ikuta_repository'],
+            repository,
             # TODO: update cmd
             cmd=['python'],
             entrypoint=['/lambda-entrypoint.sh'],
