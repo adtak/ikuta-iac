e66424376918cc3deb10d21f2d5d9f117c376877
adtak <adtak62@gmail.com>
2021-07-28 23:11:39 +0900

    feat: add ecr stack

---
+++ ikuta_iac/stack/ecr.py
@@ -0,0 +1,24 @@
+from aws_cdk import core
+from aws_cdk.aws_ecr import Repository, LifecycleRule, TagStatus
+
+
+class IkutaEcrStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create ECR
+        lifecycle_rule = LifecycleRule(
+            description="Keep only one image.",
+            max_image_count=1,
+            rule_priority=1,
+            tag_status=TagStatus.ANY,
+        )
+        repository = Repository(
+            self,
+            "ikutaRepository",
+            image_scan_on_push=False,
+            lifecycle_rules=[lifecycle_rule],
+            removal_policy=core.RemovalPolicy.DESTROY,
+            repository_name="ikuta",
+        )
+        self.repository = repository
